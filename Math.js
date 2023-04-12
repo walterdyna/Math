ef500e5b63c9de49f74d011f289a2010390eb5e5
walterdyna <alexdyna@gmail.com>
2023-04-12 00:10:41 -0300

    Aprendendo Math

---
+++ Math.js
@@ -0,0 +1,13 @@
+// Math
+
+// Math - Uma blibioteca integrada do JavaScript - que ja e nativa e pode ser usada para ajudar em operaçoes matematicas
+// Vale lembrar o nome MATH - deve ser escrito com o "M" sempre maiusculo.
+// vale lembrar um função com MATH deve sempre ter um PARENTEZES mesmo que vazio no caso do random...
+
+console.log(Math.PI) // Mostra o valor de PI
+console.log(Math.PI + 2) // mode ser usado tambem em calculos
+console.log(Math.pow(2,3)) // para calculos de potencia como 2^2
+console.log(Math.sqrt(9,2)) // usado para calco de raiz onde o primento numero que o valor a ser calculado e o segundo e o tipode de rais. no EX uma raiz cubica (simples)
+console.log(Math.random()) // usado para mostrar um numero aleatorio entre zero 0 e um 1
+console.log(Math.ceil(23.6)) // usado para fazer arrendondamento para CIMA
+console.log(Math.floor(23.6))// usado para fazer arrendondamento para BAIXO
